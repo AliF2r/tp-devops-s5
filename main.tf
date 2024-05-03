b39cb21bd67114849a1785c1a7a0d14dd967f73a
Mustapha Boudouch <mustaphaboudouch@gmail.com>
2024-05-03 14:35:24 +0200

    refactor to variables

--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
-}
-
+# Create resource group
 resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
   location = var.resource_group_location
-  name     = random_pet.rg_name.id
 }
 
 # Create virtual network
 resource "azurerm_virtual_network" "my_terraform_network" {
-  name                = "myVnet"
+  name                = var.virtual_network_name
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -17,7 +14,7 @@ resource "azurerm_virtual_network" "my_terraform_network" {
 
 # Create subnet
 resource "azurerm_subnet" "my_terraform_subnet" {
-  name                 = "mySubnet"
+  name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.1.0/24"]
@@ -25,15 +22,15 @@ resource "azurerm_subnet" "my_terraform_subnet" {
 
 # Create public IPs
 resource "azurerm_public_ip" "my_terraform_public_ip" {
-  name                = "myPublicIP"
+  name                = var.public_ip_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
 }
 
-# Create Network Security Group and rule
+# Create Network Security Group and rules
 resource "azurerm_network_security_group" "my_terraform_nsg" {
-  name                = "myNetworkSecurityGroup"
+  name                = var.network_security_group_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -64,12 +61,12 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
 
 # Create network interface
 resource "azurerm_network_interface" "my_terraform_nic" {
-  name                = "myNIC"
+  name                = var.nic_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "my_nic_configuration"
+    name                          = var.nic_configuration_name
     subnet_id                     = azurerm_subnet.my_terraform_subnet.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
@@ -94,7 +91,7 @@ resource "random_id" "random_id" {
 
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "my_storage_account" {
-  name                     = "diag${random_id.random_id.hex}"
+  name                     = "${var.storage_account_name_prefix}${random_id.random_id.hex}"
   location                 = azurerm_resource_group.rg.location
   resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
@@ -103,14 +100,16 @@ resource "azurerm_storage_account" "my_storage_account" {
 
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
-  name                  = "myVM"
+  name                  = var.vm_name
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
   size                  = "Standard_DS1_v2"
+  computer_name         = "hostname"
+  admin_username        = var.vm_admin_username
 
   os_disk {
-    name                 = "myOsDisk"
+    name                 = var.vm_os_disk_name
     caching              = "ReadWrite"
     storage_account_type = "Premium_LRS"
   }
@@ -122,11 +121,8 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
     version   = "latest"
   }
 
-  computer_name  = "hostname"
-  admin_username = var.username
-
   admin_ssh_key {
-    username   = var.username
+    username   = var.vm_admin_username
     public_key = lookup(azapi_resource_action.ssh_public_key_gen.output, "publicKey", null)
   }
 
@@ -135,29 +131,26 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   }
 }
 
-
-
+# Execute provisioning scripts
 resource "null_resource" "counter_app" {
-
   triggers = {
-    ip = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
+    ip         = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
     private_key = lookup(azapi_resource_action.ssh_public_key_gen.output, "privateKey", null)
-    user = var.username
+    user       = var.vm_admin_username
   }
 
   connection {
     type        = "ssh"
     host        = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
-    user        = var.username
+    user        = var.vm_admin_username
     private_key = lookup(azapi_resource_action.ssh_public_key_gen.output, "privateKey", null)
     timeout     = "2m"
   }
 
   provisioner "file" {
-    source = "./script.sh"
+    source      = "./script.sh"
     destination = "/tmp/script.sh"
   }
-
 
   provisioner "remote-exec" {
     inline = [
@@ -167,5 +160,3 @@ resource "null_resource" "counter_app" {
     ]
   }
 }
-
-
--- variables.tf
+++ variables.tf
@@ -1,17 +1,79 @@
+variable "resource_group_name" {
+  type        = string
+  default     = "myResourceGroup"
+  description = "The name of the resource group in which to create the resources."
+}
+
 variable "resource_group_location" {
   type        = string
-  default     = "eastus"
-  description = "Location of the resource group."
+  default     = "westeurope"
+  description = "The location/region where the resource group will be created."
 }
 
-variable "resource_group_name_prefix" {
+variable "virtual_network_name" {
   type        = string
-  default     = "rg"
-  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+  default     = "myVnet"
+  description = "The name of the virtual network."
 }
 
-variable "username" {
+variable "subnet_name" {
   type        = string
-  description = "The username for the local account that will be created on the new VM."
+  default     = "mySubnet"
+  description = "The name of the subnet."
+}
+
+
+variable "public_ip_name" {
+  type        = string
+  default     = "myPublicIP"
+  description = "The name of the public IP address."
+}
+
+
+variable "network_security_group_name" {
+  type        = string
+  default     = "myNetworkSecurityGroup"
+  description = "The name of the network security group."
+}
+
+variable "nic_name" {
+  type        = string
+  default     = "myNIC"
+  description = "The name of the network interface."
+}
+
+variable "nic_configuration_name" {
+  type        = string
+  default     = "myNicConfiguration"
+  description = "The name of the network interface configuration."
+}
+
+variable "storage_account_name_prefix" {
+  type        = string
+  default     = "diag"
+  description = "Prefix of the storage account name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable "vm_name" {
+  type        = string
+  default     = "myVM"
+  description = "The name of the virtual machine."
+}
+
+variable "vm_os_disk_name" {
+  type        = string
+  default     = "myOsDisk"
+  description = "The name of the OS disk."
+}
+
+variable "vm_computer_name" {
+  type        = string
+  default     = "hostname"
+  description = "The computer name for the new VM."
+}
+
+variable "vm_admin_username" {
+  type        = string
   default     = "azureadmin"
+  description = "The admin username for the new VM."
 }
